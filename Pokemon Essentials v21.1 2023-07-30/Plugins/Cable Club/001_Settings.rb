23ebde47f755e1780ebb418b248e0ac0c5ae9714
kass <kass@basedzone.xyz>
2023-09-22 18:35:13 -0400

    Added CableClub files

---
+++ Pokemon Essentials v21.1 2023-07-30/Plugins/Cable Club/001_Settings.rb
@@ -0,0 +1,16 @@
+module CableClub
+  HOST = "127.0.0.1"
+  PORT = 9999
+  
+  ONLINE_TRAINER_TYPE_LIST = [
+    [:POKEMONTRAINER_Red,:POKEMONTRAINER_Leaf],
+    [:PSYCHIC_M,:PSYCHIC_F],
+    [:BLACKBELT,:CRUSHGIRL],
+    [:COOLTRAINER_M,:COOLTRAINER_F]
+  ]
+  ENABLE_RECORD_MIXER = false
+  
+  # If true, Sketch fails when used.
+  # If false, Sketch is undone after battle
+  DISABLE_SKETCH_ONLINE = true
+end
